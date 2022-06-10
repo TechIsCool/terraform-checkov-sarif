1ab99c5cb8c79ac7241f691d2e5c6cc66441f121
David Beck <david.beck@invitae.com>
2022-06-10 11:49:36 -0700

    feat(checkov): testing

--- s3.tf
+++ s3.tf
@@ -1,3 +1,22 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Comments to bring this down farther
 resource "aws_s3_bucket" "foo-bucket" {
   region        = "us-east-1"
   bucket        = "foo-bucket-23ti6036"
@@ -8,6 +27,19 @@ resource "aws_s3_bucket" "foo-bucket" {
   }
   versioning {
     enabled = true
+  }
+
+  logging {
+    target_bucket = "foo-bucket-logging"
+    target_prefix = "log/"
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = "arn:blah:vlah:blah:a52190125"
+        sse_algorithm     = "aws:kms"
+      }
+    }
   }
   acl = "public"
 }
